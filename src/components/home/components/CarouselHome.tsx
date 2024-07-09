e20100d66fcbf823111ee04a3301eac75bfb5dd7
khanh228ey <khanh2282002dzz@gmail.com>
2024-07-09 17:32:48 +0700

    banner

--- src/components/home/components/CarouselHome.tsx
+++ src/components/home/components/CarouselHome.tsx
@@ -3,12 +3,13 @@ import { Image } from "antd";
 
 export default function CarouselHome() {
     return (
-        <CCarousel arrows infinite={false} autoplay autoplaySpeed={1000}>
+        <CCarousel  arrows infinite={false} autoplay autoplaySpeed={1000} >
+            <video height="500px" width="100%"  autoPlay muted loop>
+                <source src="https://nhakhoaparkway.com/wp-content/uploads/2024/03/banner-web_2800x898-1.mp4" type="video/mp4" />
+                        Your browser does not support the video tag.</video>
             <Image src="https://nhakhoaparkway.com/wp-content/uploads/2024/04/1.jpg.webp" alt="..." height="500px" width="100%" preview={false} />
-            <Image src="https://imagev3.vietnamplus.vn/w660/Uploaded/2024/mzdic/2023_03_24/Cristiano_Ronaldo_Portugal_2403.jpg.webp" alt="..." height="500px" width="100%" preview={false} />
-            <Image src="https://imagev3.vietnamplus.vn/w660/Uploaded/2024/mzdic/2023_03_24/Cristiano_Ronaldo_Portugal_2403.jpg.webp" alt="..." height="500px" width="100%" preview={false} />
-            <Image src="https://imagev3.vietnamplus.vn/w660/Uploaded/2024/mzdic/2023_03_24/Cristiano_Ronaldo_Portugal_2403.jpg.webp" alt="..." height="500px" width="100%" preview={false} />
-            <Image src="https://imagev3.vietnamplus.vn/w660/Uploaded/2024/mzdic/2023_03_24/Cristiano_Ronaldo_Portugal_2403.jpg.webp" alt="..." height="500px" width="100%" preview={false} />
+            <Image src="https://nhakhoaparkway.com/wp-content/uploads/2024/07/Coverwebdesktop-snhat-PW-1.jpg" alt="..." height="500px" width="100%" preview={false} />
+            <Image src="https://nhakhoaparkway.com/wp-content/uploads/2024/03/banner-Web-desktop.jpg.webp" alt="..." height="500px" width="100%" preview={false} />
         </CCarousel>
     );
 }
--- src/custom_antd/CCarousel.tsx
+++ src/custom_antd/CCarousel.tsx
@@ -1,13 +1,27 @@
 import { Carousel, CarouselProps } from "antd";
+import styled from "styled-components";
 import React, { ReactNode } from "react";
 
 interface CCarouselProps extends CarouselProps {
     children?: ReactNode;
 }
 
+const StyledCarousel = styled(Carousel)`
+    .slick-dots li {
+        background-color: #cccccc; /* Màu của các nút chờ */
+    }
+    .slick-dots li.slick-active {
+        background-color: #007bff; /* Màu của nút chuyển trang đang được chọn */
+    }
+`;
 const CCarousel: React.FC<CCarouselProps> = (props) => {
     const { children, ...rest } = props;
-    return <Carousel {...rest}>{children}</Carousel>;
+
+    return (
+        <StyledCarousel {...rest}>
+            {children}
+        </StyledCarousel>
+    );
 };
 
 export default CCarousel;

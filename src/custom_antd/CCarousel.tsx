ff96e1023b16842c5683334e9163421162974e54
khanh228ey <khanh2282002dzz@gmail.com>
2024-07-09 17:36:47 +0700

    a

--- src/custom_antd/CCarousel.tsx
+++ src/custom_antd/CCarousel.tsx
@@ -1,27 +1,13 @@
 import { Carousel, CarouselProps } from "antd";
-import styled from "styled-components";
 import React, { ReactNode } from "react";
 
 interface CCarouselProps extends CarouselProps {
     children?: ReactNode;
 }
 
-const StyledCarousel = styled(Carousel)`
-    .slick-dots li {
-        background-color: #cccccc; /* Màu của các nút chờ */
-    }
-    .slick-dots li.slick-active {
-        background-color: #007bff; /* Màu của nút chuyển trang đang được chọn */
-    }
-`;
 const CCarousel: React.FC<CCarouselProps> = (props) => {
     const { children, ...rest } = props;
-
-    return (
-        <StyledCarousel {...rest}>
-            {children}
-        </StyledCarousel>
-    );
+    return <Carousel {...rest}>{children}</Carousel>;
 };
 
 export default CCarousel;

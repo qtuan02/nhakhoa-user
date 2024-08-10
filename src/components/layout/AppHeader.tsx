c1c3f9c1c3d11f85b1f6b9f9fefca8f322fafa4e
qtuan02 <quoctuan200702@gmail.com>
2024-08-10 13:28:11 +0700

    edit code v1

--- src/components/layout/AppHeader.tsx
+++ src/components/layout/AppHeader.tsx
@@ -20,19 +20,10 @@ export default function AppHeader() {
     const language = getLanguage();
 
     const items = [
-        customItemMenu(<div className='w-[calc(100vw-70vw)]'><Link className="text-[16px] text-[#000] font-[500] inline-block w-full py-1" href="/gioi-thieu">{t('about')}</Link></div>, "/gioi-thieu"),
-        customItemMenu(<div className='w-[calc(100vw-70vw)]'><Link className="text-[16px] text-[#000] font-[500] inline-block w-full py-1" href="/dich-vu">{t('service')}</Link></div>, "/dich-vu"),
-        customItemMenu(<div className='w-[calc(100vw-70vw)]'><Link className="text-[16px] text-[#000] font-[500] inline-block w-full py-1" href="/dat-lich">{t('appointment')}</Link></div>, "/gioi-thieu"),
+        customItemMenu(<Link className="text-[16px] font-[500] px-3" href="/trang-chu">{t('home')}</Link>, "/trang-chu"),
+        customItemMenu(<Link className="text-[16px] font-[500] px-3" href="/gioi-thieu">{t('about')}</Link>, "/gioi-thieu"),
+        customItemMenu(<Link className="text-[16px] font-[500] px-3" href="/dich-vu">{t('service')}</Link>, "/dich-vu"),
     ];
-
-    const handleMenuClick: MenuProps['onClick'] = (e) => {
-        console.log('click', e);
-    };
-
-    const menuProps = {
-        items,
-        onClick: handleMenuClick,
-    };
 
     const handleChangeLanguage = () => {
         const newLanguage = language === "en" ? "vi" : "en";
@@ -41,73 +32,47 @@ export default function AppHeader() {
     }
 
     return (
-        <Header className="!bg-[#fff] border-b border-[#f1f1f1] shadow-xl fixed top-0 left-0 w-full z-50 !px-0 md:!px-[64px]">
-            <div className='h-full hidden md:block'>
-                <CRow className="h-full">
-                    <CCol xs={4} className="h-[64px]">
-                        <a href="/"><Avatar shape="square" size={64} src="/logo.png" /></a>
-                    </CCol>
-                    <CCol xs={12} className="h-full">
-                        <CMenu
-                            className="h-full"
-                            mode={"horizontal"}
-                            items={items}
-                            selectedKeys={[choose]}
-                        />
-                    </CCol>
-                    <CCol xs={8} className="h-full">
-                        <CRow>
-                            <CCol xs={16}>
-                                <CRow>
-                                    <CCol xs={12}>
-                                        <CButton type="primary" className="rounded-lg" link="/dat-lich" icon={<FontAwesomeIcon icon={faCalendarDays} />}>
-                                            <span className="font-bold">{t('appointment')}</span>
+        <Header className="!bg-[#fff] border-b border-[#f1f1f1] shadow-xl fixed top-0 left-0 w-full z-50">
+            <CRow className="h-full">
+                <CCol xs={4} className="h-[64px]">
+                    <a href="/"><Avatar shape="square" size={64} src="/logo.png" /></a>
+                </CCol>
+                <CCol xs={12} className="h-full">
+                    <CMenu
+                        className="h-full"
+                        mode={"horizontal"}
+                        items={items}
+                        selectedKeys={[choose]}
+                    />
+                </CCol>
+                <CCol xs={8} className="h-full">
+                    <CRow>
+                        <CCol xs={16}>
+                            <CRow>
+                                <CCol xs={12}>
+                                    <CButton type="primary" className="rounded-lg" link="/dat-lich" icon={<FontAwesomeIcon icon={faCalendarDays} />}>
+                                        <span className="font-bold">{t('appointment')}</span>
+                                    </CButton>
+                                </CCol>
+                                <CCol xs={12}>
+                                    <a href="tel:0338230318">
+                                        <CButton type="primary" danger className="rounded-lg" icon={<FontAwesomeIcon icon={faPhone} />}>
+                                            <span className="font-bold">033 8230 318</span>
                                         </CButton>
-                                    </CCol>
-                                    <CCol xs={12}>
-                                        <a href="tel:0338230318">
-                                            <CButton type="primary" danger className="rounded-lg" icon={<FontAwesomeIcon icon={faPhone} />}>
-                                                <span className="font-bold">033 8230 318</span>
-                                            </CButton>
-                                        </a>
-                                    </CCol>
-                                </CRow>
-                            </CCol>
-                            <CCol xs={8}>
-                                <CRow justify="end">
-                                    <CCol>
-                                        <CButton type="link" onClick={() => handleChangeLanguage()} icon={<Avatar shape="square" src={language === "en" ? "/icon_vie.png" : "/icon_eng.png"} />} iconPosition="end"><span className="!font-[700] !text-[#000]">{language === "en" ? "VIE" : "ENG"}</span></CButton>
-                                    </CCol>
-                                </CRow>
-                            </CCol>
-                        </CRow>
-                    </CCol>
-                </CRow>
-            </div>
-            <div className='h-full md:hidden w-full'>
-                <CRow className='h-full px-5' justify="space-between">
-                    <CCol>
-                        <a href="/"><Avatar shape="square" size={64} src="/logo.png" /></a>
-                    </CCol>
-                    <CCol>
-                        <Flex align='center'>
-                            <CCol>
-                                <CButton className='mr-4' type="link" onClick={() => handleChangeLanguage()} icon={<Avatar shape="square" src={language === "en" ? "/icon_vie.png" : "/icon_eng.png"} />} iconPosition="end"></CButton>
-                            </CCol>
-                            <CCol>
-                                <a href="tel:0338230318" className='mr-2'>
-                                    <CButton type="primary" danger className="rounded-lg" icon={<FontAwesomeIcon icon={faPhone} />} />
-                                 </a>
-                            </CCol>
-                            <CCol className='flex items-center'>
-                                <Dropdown menu={menuProps} className='border px-4 py-3 rounded-lg cursor-pointer' trigger={['click']}>
-                                    <FontAwesomeIcon icon={faBars} />
-                                </Dropdown>
-                            </CCol>
-                        </Flex>
-                    </CCol>
-                </CRow>
-            </div>
+                                    </a>
+                                </CCol>
+                            </CRow>
+                        </CCol>
+                        <CCol xs={8}>
+                            <CRow justify="end">
+                                <CCol>
+                                    <CButton type="link" onClick={() => handleChangeLanguage()} icon={<Avatar shape="square" src={language === "en" ? "/icon_vie.png" : "/icon_eng.png"} />} iconPosition="end"><span className="!font-[700] !text-[#000]">{language === "en" ? "VIE" : "ENG"}</span></CButton>
+                                </CCol>
+                            </CRow>
+                        </CCol>
+                    </CRow>
+                </CCol>
+            </CRow>
         </Header>
     );
 }
